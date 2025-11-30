b0fbb9b4486097600333d375f2ca728558d83bca
MoritzValentin <79050824+MoritzValentin@users.noreply.github.com>
2025-11-30 11:30:46 +0100

    testing job

---
+++ testing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import transformers
+import datasets
+import torch
+
+
+print("testing script")
+print(pd.__version__)
+print(transformers.__version__)
+print(datasets.__version__)
+print(torch.__version__)
+print("testing script done!")
